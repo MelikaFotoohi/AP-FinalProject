2a03fb6451450d8ba36953b777289b86a33cad3a
golnoushnb <80672398+golnoushnb@users.noreply.github.com>
2021-07-15 10:09:06 +0430

    Update mainwindow.cpp

--- codes/mainwindow/mainwindow.cpp
+++ codes/mainwindow/mainwindow.cpp
@@ -6,10 +6,24 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->centralWidget()->setStyleSheet("background-image:url(\"/catan.jpeg\"); background-position: center;");
+    QPixmap pix("/catan-logo.png");
+    ui->label->setPixmap(pix);
+    this->setFixedSize(500 , 350);
+    connect(ui->pushButton , SIGNAL(clicked()) , this , SLOT(openLoginPage()));
+
+}
+void MainWindow::openLoginPage(){
+     Login *lgn = new Login();
+     this->close();
+     lgn->show();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
 
