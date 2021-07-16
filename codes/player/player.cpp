7d250133af575b1be6c58192c4d0bd4b30c5f3c8
Parto <79199073+sabzparto@users.noreply.github.com>
2021-07-16 17:58:25 +0430

    Create player.cpp

---
+++ codes/player/player.cpp
@@ -0,0 +1,19 @@
+#include "player.h"
+
+Player::Player(QWidget *parent) : QWidget(parent)
+{
+
+}
+void Player::draw(){
+
+}
+int Player::update(){
+
+}
+//  map<tileNum, tileType> buildingResources;
+QVector<ResourceCard> Player::giveCardToBank(){
+
+}
+void Player::changeRobber(){
+
+}
