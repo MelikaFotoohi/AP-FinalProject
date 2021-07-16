fb9915007ef357a260f83c3653c50df0dc59246d
Parto <79199073+sabzparto@users.noreply.github.com>
2021-07-16 17:53:00 +0430

    Update location.cpp

--- codes/location/location.cpp
+++ codes/location/location.cpp
@@ -1,18 +1,25 @@
 #include "location.h"
 
 
-Location::Location(int x, int y, location locationType)
+Location::Location(int _x, int _y, locationType _type)
 {
-
+    x = _x;
+    y = _y;
+    type = _type;
 }
 Location::Location(){
 
 }
-
 QVector<Location> Location::getADjacent(){
 
-}
 
+}
+int Location::getX(){
+    return x;
+}
+int Location::getY(){
+    return y;
+}
 void Location::findAdjacent(){
 
 }
