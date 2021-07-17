0ae230739af12f167bc24f9767910645a90454a8
MelikaFotoohi <80758012+MelikaFotoohi@users.noreply.github.com>
2021-07-17 23:40:30 +0430

    Update tradewindow.cpp
    new trade window with new ui

--- codes/trade window/tradewindow.cpp
+++ codes/trade window/tradewindow.cpp
@@ -4,19 +4,10 @@
 tradewindow::tradewindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::tradewindow)
 {
     ui->setupUi(this);
-    this->setFixedSize(450, 382);
-    this->centralWidget()->setStyleSheet( "background-image:url(\"/Users/AHFB/Favorites/Desktop/catan images/background/22.jpg\"); background-position: center;");
-
-    QPixmap pixmap("/Users/AHFB/Favorites/Desktop/catan images/trade.png");
-    QIcon ButtonIcon(pixmap);
-    setIconForPushButton(pixmap, ButtonIcon, ui->pushButton);
-
-//    QEventLoop conditionCheckLoop;
-//    QObject::connect(ui->pushButton, SIGNAL(clicked()), &conditionCheckLoop, SLOT(waitForCheckingConditions()));
-//    conditionCheckLoop.exec();
+    this->setFixedSize(371, 373);
+    this->centralWidget()->setStyleSheet( "background-image:url(\"C:/Users/AHFB/Favorites/Desktop/catan images/background/22.jpg\"); background-position: center;");
 
     connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(waitForCheckingConditions()));
-    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(TradeRequest()));
 }
 
 tradewindow::~tradewindow()
@@ -34,8 +25,8 @@ void tradewindow::setIconForPushButton(QPixmap pix, QIcon icon, QPushButton* but
 void tradewindow::TradeRequest()
 {
     MainWindow *x = new MainWindow;
-    x->show();
     this->close();
+    x->show();
 }
 
 void tradewindow::waitForCheckingConditions()
@@ -52,7 +43,8 @@ void tradewindow::waitForCheckingConditions()
                 tr("You cannot just recieve cards. It must be two-sided."));
         }
 
-        if((ui->spinBox_6->value() == 0 && ui->spinBox_7->value() == 0 && ui->spinBox_8->value() == 0 && ui->spinBox_9->value() == 0
+
+        else if((ui->spinBox_6->value() == 0 && ui->spinBox_7->value() == 0 && ui->spinBox_8->value() == 0 && ui->spinBox_9->value() == 0
              && ui->spinBox_10->value() == 0) && (ui->spinBox->value() != 0 || ui->spinBox_2->value() != 0
                 || ui->spinBox_3->value() != 0 || ui->spinBox_4->value() != 0 || ui->spinBox_5->value() != 0))
         {
@@ -60,31 +52,49 @@ void tradewindow::waitForCheckingConditions()
                 tr("You cannot just get cards. It must be two-sided!"));
         }
 
-        if(ui->spinBox->value() == 4 || ui->spinBox_2->value() == 4 || ui->spinBox_3->value() == 4
-            || ui->spinBox_4->value() == 4 || ui->spinBox_5->value() == 4)
+
+        else if((ui->spinBox->value() == 4 || ui->spinBox_2->value() == 4 || ui->spinBox_3->value() == 4
+                || ui->spinBox_4->value() == 4 || ui->spinBox_5->value() == 4) && (ui->checkBox->isChecked() == true
+                    || ui->checkBox_2->isChecked() == true || ui->checkBox_3->isChecked() == true))
         {
-            if(ui->checkBox->isChecked() == true || ui->checkBox_2->isChecked() == true || ui->checkBox_3->isChecked() == true)
-            {
-                QMessageBox::warning(this, tr("Warning"),
-                    tr("You have to exchange 4 cards only with the bank!"));
-            }
+            QMessageBox::warning(this, tr("Warning"),
+                tr("You have to exchange 4 cards only with the bank!"));
+        }
 
-            if(sumOfRecievedCards != 1)
-            {
-                QMessageBox::warning(this, tr("Warning"),
-                    tr("You have to give 4 cards to the bank and receive one card in return!"));
-            }
-       }
 
-        else if(ui->checkBox_4->isChecked() == true ) {
+        else if((ui->spinBox->value() == 4 || ui->spinBox_2->value() == 4 || ui->spinBox_3->value() == 4
+                 || ui->spinBox_4->value() == 4 || ui->spinBox_5->value() == 4) && sumOfRecievedCards != 1)
+        {
+            QMessageBox::warning(this, tr("Warning"),
+                tr("You have to give 4 cards to the bank and receive one card in return!"));
+        }
+
+
+        else if(ui->checkBox_4->isChecked() == true && sumOfRecievedCards ==1 && sumOfGivenCards!=4)
+        {
             QMessageBox::warning(this, tr("Warning"),
                 tr("You must have 4 cards to exchange with bank!"));
         }
 
-        if ((sumOfGivenCards!=0 && sumOfRecievedCards !=0 &&  ui->checkBox->isChecked() == false && ui->checkBox_2->isChecked() == false
+
+        else if ((sumOfGivenCards!=0 && sumOfRecievedCards !=0 &&  ui->checkBox->isChecked() == false && ui->checkBox_2->isChecked() == false
                         && ui->checkBox_3->isChecked() == false && ui->checkBox_4->isChecked() == false))
         {
             QMessageBox::warning(this, tr("Warning"),
                 tr("You have to choose players who you want to change card with!"));
         }
+
+
+        else if(sumOfGivenCards==0 && sumOfRecievedCards==0 && ui->checkBox->isChecked() == false && ui->checkBox_2->isChecked() == false
+                && ui->checkBox_3->isChecked() == false && ui->checkBox_4->isChecked() == false)
+        {
+            QMessageBox messageBox(QMessageBox::Question,tr("close window"),tr("Exit the trade window?"),QMessageBox::Yes | QMessageBox::No,NULL);
+            messageBox.setButtonText(QMessageBox::Yes, tr("Yes"));
+            messageBox.setButtonText(QMessageBox::No, tr("No"));
+
+            if( messageBox.exec() == QMessageBox::Yes)
+                TradeRequest();
+        }
+
+        else TradeRequest();
 }
