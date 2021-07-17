15306a230028217ac93ad98f72a7568726a0b33c
Parto <79199073+sabzparto@users.noreply.github.com>
2021-07-18 00:10:38 +0430

    update functions

--- codes/player/player.h
+++ codes/player/player.h
@@ -1,33 +1,67 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <QWidget>
+#include <QMainWindow>
 #include <QVector>
+#include <QTcpSocket>
+#include <QByteArray>
+#include <QJsonArray>
+
 #include "resourcecard.h"
 #include "developmentcard.h"
 #include "building.h"
 #include "trade.h"
 #include "map.h"
-class Player : public QWidget
+
+namespace Ui {
+class Player;
+}
+
+
+class Player : public QMainWindow
 {
     Q_OBJECT
 public:
-    Player(QWidget *parent = nullptr);
-    //Player();
-    void draw();
-    int update();
-  //  map<tileNum, tileType> buildingResources;
-    QVector<ResourceCard> giveCardToBank();
+    Ui::Player *ui;
+    Player(Map* _map,QWidget *parent = nullptr);
+    bool buySettlement();
+    bool buyRoad();
+    bool buyCity();
+    void addSettlement(QPushButton*);
+    void updateToCity(QPushButton*);
+    void addRoad(QPushButton*);
+    void getDevelopmentCard();
+    void getResourceCard(tileType);
     void changeRobber();
+    bool isSettlement(QPushButton*);
+    void setIconForPushButton(QPixmap pix, QIcon icon, QPushButton* button);
+    QVector<QPushButton*> getSettlements();
+    int isAdjacent(Tile* tile);
+
 private:
-    QVector<ResourceCard> resource;
-    QVector<DevelopmentCard> development;
-    QVector<buildingType> buildings;
- //   Trade trade;
-    int victoryPoint;
+    Map* map;
+
+    QVector<QPushButton*> settlements;
+    QVector<QPushButton*> cities;
+    QVector<QPushButton*> roads;
+
+    int fieldCard=0 , stoneCard=0, clayCard=0, pastureCard=0, forestCard=0;
+    int victoryPointCard=0, KnightCard=0, roadBuildingCard=0, yearOfPlentyCard=0, monopolyCard=0;
+    int bridgePiece=0, roadPiece=15, cityPiece=4, settlementPiece=5;
+    int totalPoint;
     int PlayerID;
     int sumOfDices;
+
+    QTcpSocket* clientSocket;
+public slots:
+    void goToTradeWindow();
+    void showRoads();
+    void showSetllement();
+    void showCity();
+
 signals:
+
+
 
 };
 
