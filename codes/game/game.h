cc4563526183a40a25c158462613c469a1114721
Parto <79199073+sabzparto@users.noreply.github.com>
2021-07-18 00:03:31 +0430

    add functions

--- codes/game/game.h
+++ codes/game/game.h
@@ -2,24 +2,58 @@
 #define GAME_H
 
 #include <QVector>
-
+#include <QTcpServer>
 #include "player.h"
 #include "map.h"
 #include "bank.h"
+#include "thread.h"
+#include "dice.h"
+#include <QSignalMapper>
 
-
-class Game
+class Game : public QTcpServer
 {
+    Q_OBJECT
 public:
-    Game();
+    Game(QObject *parent = 0);
+    void startGame();
+
     void checkWining();
-    int playerTurn();
+   // void startServer();
+    void Print(Ui::Player *ui);
+    void settlementConnect();
+    void roadConnect();
+
+
 private:
+    QSignalMapper *signalMapper;
     QVector<Player*> players;
-    Map map;
-    Bank bank;
+    Player* player1;
+    Player* player2;
+    Player* player3;
+    Player* player4;
+
+    Map* map;
+    Bank* bank;
+    Dice* dice;
     bool start;
 
+
+    static int diceNum;
+    static int currPlayer;
+    static int gameStarted_;
+    static int player_;
+    static int builds_;
+
+public slots:
+    void giveCardRoll();
+    void playerTurn();
+    void gotoRoll();
+    void slectedBuilding(QObject*);
+    void slectedSetllement(QObject* p);
+    void slectedCity(QObject* p);
+    void slectedRoad(QObject* p);
+protected:
+  //  void incomingConnection(qintptr socketDescriptor);
 
 };
 
