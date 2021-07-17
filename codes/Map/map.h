b44f72576d5fa59393fda2ae4311fa46d78d3e5d
MelikaFotoohi <80758012+MelikaFotoohi@users.noreply.github.com>
2021-07-18 00:01:46 +0430

    Update map.h
    new map file

--- codes/Map/map.h
+++ codes/Map/map.h
@@ -6,6 +6,8 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QDebug>
+#include <QPair>
+#include <QSignalMapper>
 
 #include "tile.h"
 #include "robber.h"
@@ -19,18 +21,39 @@ class Map : public QMainWindow
     Q_OBJECT
 
 public:
+    Ui::Map *ui;
     explicit Map(QWidget *parent = nullptr);
+    void randomMap(QVector<QPixmap>,QVector<QPixmap> pixs_nums);
     tileType TileType(int n);
-    QVector<tileType> getAdjacent(QPushButton* p);
+    QVector<Tile*> getTiles();
+    QVector<QPushButton*> getAvailableVertexs();
+    QVector<QPushButton*> getAvailableEdges();
+    void setHide(QVector<QPushButton*>);
+    void setUnHide(QVector<QPushButton*>);
+    void removeVertex(QPushButton*);
+    void removeEdge(QPushButton*);
+    QVector<QLabel*> rollNumTile(int num);
+    bool numbers(int pixNum , QVector<QPair<int,int>> tile_nums);
     ~Map();
 
+public slots:
+    QVector<QPair<tileType,int>> getAdjacentVertex(QPushButton* p);
+    QVector<QPair<int,int>> getAdjacentTile(QObject* p);
+    void moveRobber(int);
+
 private:
+    QSignalMapper *signalMapper;
+
     QVector<Tile*> tiles;
     QVector<QLabel*> labels;
+
     QVector<QPushButton*> vertexs;
     QVector<QPushButton*> edges;
+    QVector<QPushButton*> tokens;
+    QVector<QPushButton*> vertexsAvailable;
+    QVector<QPushButton*> edgesAvailable;
     Robber robber();
-    Ui::Map *ui;
+
 };
 
 #endif // MAP_H
