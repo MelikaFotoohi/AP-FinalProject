71fc4c0c26bf2f929c792c96d70b20fffd9060da
Parto <79199073+sabzparto@users.noreply.github.com>
2021-07-22 00:20:59 +0430

    Update dice.cpp

--- codes/dice/dice.cpp
+++ codes/dice/dice.cpp
@@ -7,16 +7,15 @@ Dice::Dice(Map* _map,QWidget *parent) : QWidget(parent)
 {
     map = _map;
 }
-void Dice::roll(){
-    QPixmap Dice1("/dice_1.png");
-    QPixmap Dice2("/dice_2.png");
-    QPixmap Dice3("/dice_3.png");
-    QPixmap Dice4("/dice_4.png");
-    QPixmap Dice5("/dice_5.png");
-    QPixmap Dice6("/dice_6.png");
+int Dice::roll(){
+    QPixmap Dice1(":/new/prefix1/dice/dice_1.png");
+    QPixmap Dice2(":/new/prefix1/dice/dice_2.png");
+    QPixmap Dice3(":/new/prefix1/dice/dice_3.png");
+    QPixmap Dice4(":/new/prefix1/dice/dice_4.png");
+    QPixmap Dice5(":/new/prefix1/dice/dIce_5.png");
+    QPixmap Dice6(":/new/prefix1/dice/dice_6.png");
 
     QVector<QPixmap> pix{Dice1 , Dice2 , Dice3 , Dice4 , Dice5 , Dice6};
-    srand(time(NULL));
     int i = rand() % 6;
     diceNum = i + 1;
     QIcon button1(pix[i]);
@@ -25,9 +24,10 @@ void Dice::roll(){
     map->ui->Dice_1->setIconSize(pix[i].rect().size());
 
     i = rand() % 6;
-    diceNum = i + 1;
+    diceNum += i + 1;
     QIcon button2(pix[i]);
     map->ui->Dice_2->setIcon(button2);
 
     map->ui->Dice_2->setIconSize(pix[i].rect().size());
+    return diceNum;
 }
