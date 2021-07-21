2bece9ccce4ca3b3214157a193f0c99db8306e50
MelikaFotoohi <80758012+MelikaFotoohi@users.noreply.github.com>
2021-07-22 00:27:56 +0430

    Update request.cpp
    final cpp file for erequest

--- codes/request/request.cpp
+++ codes/request/request.cpp
@@ -10,6 +10,16 @@ Request::Request(QVector<QPair<tileType,QPair<int,int>>> _cards,Player* _other,P
     me = _me;
     cards = _cards;
     ui->setupUi(this);
+    setFixedSize(459 , 200);
+    setWindowTitle("Request");
+
+    QPixmap bkgnd(":/new/prefix1/desert.jpeg");
+    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Window, bkgnd);
+    this->setPalette(palette);
+
+    ui->pushButton_3->setText("New request from " + other->getName() + " to " + me->getName());
     ui->clayGiveNum->setText(QString::number(cards[0].second.second));
     ui->clayRecNum->setText(QString::number(cards[0].second.first));
 
@@ -25,6 +35,23 @@ Request::Request(QVector<QPair<tileType,QPair<int,int>>> _cards,Player* _other,P
     ui->pastureGiveNum->setText(QString::number(cards[4].second.second));
     ui->pastureRecNum->setText(QString::number(cards[4].second.first));
 
+    QPixmap pixmap_field(":/new/prefix1/cut/grain.jpg");
+    QPixmap pixmap_clay(":/new/prefix1/cut/brick2.jpg");
+    QPixmap pixmap_forest(":/new/prefix1/cut/lumber.jpg");
+    QPixmap pixmap_stone(":/new/prefix1/cut/ors.jpg");
+    QPixmap pixmap_pasture(":/new/prefix1/cut/wool.jpg");
+
+    ui->label->setPixmap(pixmap_clay);
+    ui->label->setMask(pixmap_clay.mask());
+    ui->label_2->setPixmap(pixmap_field);
+    ui->label_2->setMask(pixmap_field.mask());
+    ui->label_3->setPixmap(pixmap_forest);
+    ui->label_3->setMask(pixmap_forest.mask());
+    ui->label_4->setPixmap(pixmap_stone);
+    ui->label_4->setMask(pixmap_stone.mask());
+    ui->label_5->setPixmap(pixmap_pasture);
+    ui->label_5->setMask(pixmap_pasture.mask());
+
     QPixmap pixmap_approve(":/new/prefix1/icons/icon_approve.jpg");
     QIcon Button_approve(pixmap_approve);
     ui->ok->setIcon(Button_approve);
@@ -33,8 +60,8 @@ Request::Request(QVector<QPair<tileType,QPair<int,int>>> _cards,Player* _other,P
 
     QPixmap pixmap_cross(":/new/prefix1/icons/icon_cross.jpg");
     QIcon Button_cross(pixmap_cross);
-    ui->ok->setIcon(Button_cross);
-    ui->ok->setIconSize(pixmap_cross.rect().size());
+    ui->cancel->setIcon(Button_cross);
+    ui->cancel->setIconSize(pixmap_cross.rect().size());
 
     connect(ui->ok,SIGNAL(clicked()),this,SLOT(ok()));
     connect(ui->cancel,SIGNAL(clicked()),this,SLOT(cancel()));
